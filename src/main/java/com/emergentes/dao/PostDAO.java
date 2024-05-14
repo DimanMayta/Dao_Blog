0473b237d7d1d8a2beedfffa10bb42207a3b0cc9
Diman Mayta <maytadiman20@gmail.com>
2024-05-14 21:01:38 +0200

    version 1.0 dao blog

---
+++ src/main/java/com/emergentes/dao/PostDAO.java
@@ -0,0 +1,16 @@
+package com.emergentes.dao;
+
+import com.emergentes.modelo.Post;
+import java.util.List;
+
+/**
+ *
+ * @author Diman
+ */
+public interface PostDAO {
+    public void insert(Post posteo) throws Exception;
+    public void update(Post posteo) throws Exception;
+    public void delete(int id) throws Exception;
+    public Post getById(int id) throws Exception;
+    public List<Post> getAll() throws Exception;
+}
